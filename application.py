4cbff24f76253803484f983fa6ca8de8060e92ca
Michael Suo <suo@fb.com>
2021-12-10 17:30:12 -0800

    enable response compresion

--- application.py
+++ application.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_compress import Compress
 
 import hud
 import commit
 
 
 application = Flask(__name__)
+
+# Compress responses. The HUD is a really big HTML page, (~2MB) which actually
+# takes a bit of time to send. Enabling compression with default settings
+# reduces the size drastically (~40KB), which improves user-perceived perf.
+Compress(application)
 
 
 @application.route("/")
