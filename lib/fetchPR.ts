0f468be7497b73c3704b20bc150ff7e611efed3b
Michael Suo <suo@fb.com>
2022-01-19 11:53:25 -0800

    Some tweaks to PR view
    I had to change some stuff about the underlying data backend (`pull_request` now only reflects the *latest* state in GH, similar to the GH API), so need to fix up PR data fetching to work with that.
    
    Other minor changes/improvements
    - Which commit we're on is actually that important (almost always you want latest) so demoting it from a header element to a regular div.
    - The router's query struct is empty pre-hydration, so previously we were sending a bad request on first load, fixed that.
    - The <select> element did not have a default value previously, now it reflects the sha you are currently looking at.

--- lib/fetchPR.ts
+++ lib/fetchPR.ts
@@ -1,34 +1,39 @@
 import getRocksetClient from "./rockset";
 import { PRData } from "./types";
 
-export default async function fetchPR(pr: string): Promise<PRData[]> {
+export default async function fetchPR(pr: string): Promise<{ prData: PRData }> {
   const rocksetClient = getRocksetClient();
-  const PRQuery = await rocksetClient.queryLambdas.executeQueryLambda(
-    "commons",
-    "prs_query",
-    "eea6932288753f6d",
-    {
-      parameters: [
-        {
-          name: "pr",
-          type: "int",
-          value: pr,
-        },
-      ],
-    }
-  );
-  const shaSet = new Set();
-  const results: PRData[] = [];
+  const [prQuery, commitHistoryQuery] = await Promise.all([
+    rocksetClient.queryLambdas.executeQueryLambdaByTag(
+      "commons",
+      "pr_query",
+      "latest",
+      {
+        parameters: [
+          {
+            name: "pr",
+            type: "int",
+            value: pr,
+          },
+        ],
+      }
+    ),
+    rocksetClient.queryLambdas.executeQueryLambdaByTag(
+      "commons",
+      "pr_commit_history_query",
+      "latest",
+      {
+        parameters: [
+          {
+            name: "pr",
+            type: "int",
+            value: pr,
+          },
+        ],
+      }
+    ),
+  ]);
+  const prDataResult = prQuery.results![0];
 
-  (PRQuery.results ?? []).map(({ sha, event_time, commit, title }) => {
-    if (!shaSet.has(sha)) {
-      const shaString = sha as string;
-      const eventTime = event_time as string;
-      const firstLine = commit.indexOf("\n");
-      const commitTitle: string = commit.slice(0, firstLine);
-      shaSet.add(shaString);
-      results.push({ sha: shaString, eventTime, commitTitle, title });
-    }
-  });
-  return results;
+  return { ...prDataResult, shas: commitHistoryQuery.results! };
 }
--- pages/api/pr/[prNumber].ts
+++ pages/api/pr/[prNumber].ts
@@ -3,7 +3,7 @@ import fetchPR from "lib/fetchPR";
 import { PRData } from "lib/types";
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<PRData[]>
+  res: NextApiResponse<PRData>
 ) {
   res.status(200).json(await fetchPR(req.query.prNumber as string));
 }
